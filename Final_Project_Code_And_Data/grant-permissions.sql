388873807d678baaeeddd0e4d28544ad9f969261
jennie6 <jenniechung6@gmail.com>
2022-03-13 22:41:23 -0600

    Updated the grant-permissions.sql file to use our database

--- Final_Project_Code_And_Data/grant-permissions.sql
+++ Final_Project_Code_And_Data/grant-permissions.sql
@@ -1,1 +1,9 @@
 -- CS 121 Winter 2022 Final Project
+-- Part F: MySQL Users and Permissions
+
+CREATE USER 'bookretailer'@'localhost' IDENTIFIED BY '8sXjJK';
+CREATE USER 'bookreader'@'localhost' IDENTIFIED BY '4qY4us';
+-- Can add more users or refine permissions
+GRANT ALL PRIVILEGES ON booksdb.* TO 'bookretailer'@'localhost';
+GRANT SELECT ON booksdb.* TO 'bookreader'@'localhost';
+FLUSH PRIVILEGES;
